26bd71ab2f7ef385e0409496e0d8d3e9e86cfc7f
m-ahiru <strafearbeit@gmail.com>
2025-08-09 15:21:05 +0200

    fix

--- gui/config.h
+++ gui/config.h
@@ -6,6 +6,7 @@ extern int MAX_DELAY_US;
 extern int TRIGGER_CODE;
 extern char MOUSE_EVENT_PATH[256];
 
+const char *get_config_path(void);
 void load_config();
 void save_config();
 void ensure_config_dir_exists();
